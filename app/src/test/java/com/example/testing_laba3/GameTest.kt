57a601d227e6f260cc99178e15acf1e2c5c64964
grozaPelmeney <nfs2.01@mail.ru>
2023-04-13 17:29:52 +0700

    add tests to check can player make move and get position if can

--- app/src/test/java/com/example/testing_laba3/GameTest.kt
+++ app/src/test/java/com/example/testing_laba3/GameTest.kt
@@ -4,16 +4,49 @@ import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class GameTest {
-    //Тест для проверки, может ли игрок сделать ход
-    @Test
-    fun `check player can make move`() {
-        val x = 1
-        val o = 2
+    private val x = 1
+    private val o = 2
 
+    //Тесты для проверки, может ли игрок сделать ход
+    @Test
+    fun `check player can make move in empty board`() {
         val board = GameEngine.getEmptyBoard()
+
         val player = Player()
 
-        val expected = true
+        val expected = Pair(0, 0)
+        val actual = player.canMakeMove(board)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `check player can make move in part filled board`() {
+        val board = arrayListOf(
+            arrayListOf(x, o, o),
+            arrayListOf(o, o, x),
+            arrayListOf(x, 0, x)
+        )
+
+        val player = Player()
+
+        val expected = Pair(1, 1)
+        val actual = player.canMakeMove(board)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `check player can make move in filled board`() {
+        val board = arrayListOf(
+            arrayListOf(x, o, o),
+            arrayListOf(o, o, x),
+            arrayListOf(x, x, o)
+        )
+
+        val player = Player()
+
+        val expected = null
         val actual = player.canMakeMove(board)
 
         assertEquals(expected, actual)
