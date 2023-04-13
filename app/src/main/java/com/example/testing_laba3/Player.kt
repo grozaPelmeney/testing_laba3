c9f7142d4c3e279b747380f7bc9f8e699dcaef26
grozaPelmeney <nfs2.01@mail.ru>
2023-04-13 20:21:36 +0700

    make random choise in canMakeMove() method

--- app/src/main/java/com/example/testing_laba3/Player.kt
+++ app/src/main/java/com/example/testing_laba3/Player.kt
@@ -4,19 +4,17 @@ class Player {
     var figure = 0
 
     fun canMakeMove(board: ArrayList<ArrayList<Int>>): Pair<Int, Int>? {
-        var pos: Pair<Int, Int>? = null
+        val emptyFields = mutableListOf<Pair<Int, Int>>()
 
         for (i in 0 until board.size) {
             for (j in 0 until board[i].size) {
                 if (board[i][j] == 0) {
-                    pos = Pair(i, j)
-                    break
+                    emptyFields.add(Pair(i, j))
                 }
             }
-            if (pos != null) break
         }
 
-        return pos
+        return if (emptyFields.isEmpty()) null else emptyFields.random()
     }
 
     fun makeMove(pos: Pair<Int, Int>, board: ArrayList<ArrayList<Int>>) {
