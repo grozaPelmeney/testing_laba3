30fa34f133685046e5355d42db06253f053404d5
grozaPelmeney <nfs2.01@mail.ru>
2023-04-13 17:11:52 +0700

    add realisation for canMakeMove() method in Player class

--- app/src/main/java/com/example/testing_laba3/Player.kt
+++ app/src/main/java/com/example/testing_laba3/Player.kt
@@ -1,7 +1,18 @@
 package com.example.testing_laba3
 
 class Player {
-    fun canMakeMove(board: ArrayList<ArrayList<Int>>) {
+    var figure = 0
 
+    fun canMakeMove(board: ArrayList<ArrayList<Int>>): Boolean {
+        var canMakeMove = false
+        for (i in 0 until board.size) {
+            for (j in 0 until board[i].size) {
+                canMakeMove = board[i][j] == 0
+                if (canMakeMove) break
+            }
+            if (canMakeMove) break
+        }
+
+        return canMakeMove
     }
 }
