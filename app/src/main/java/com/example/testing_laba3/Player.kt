4ecc3ab5014f6e46d776d5b1f4324ac0b3ea2568
grozaPelmeney <nfs2.01@mail.ru>
2023-04-13 16:59:46 +0700

    add test to check can player make move (with empty method in Player class because project isn`t building without it)

--- app/src/main/java/com/example/testing_laba3/Player.kt
+++ app/src/main/java/com/example/testing_laba3/Player.kt
@@ -1,4 +1,7 @@
 package com.example.testing_laba3
 
 class Player {
+    fun canMakeMove(board: ArrayList<ArrayList<Int>>) {
+
+    }
 }
--- app/src/test/java/com/example/testing_laba3/GameTest.kt
+++ app/src/test/java/com/example/testing_laba3/GameTest.kt
@@ -1,4 +1,21 @@
 package com.example.testing_laba3
 
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class GameTest {
+    //Тест для проверки, может ли игрок сделать ход
+    @Test
+    fun `check player can make move`() {
+        val x = 1
+        val o = 2
+
+        val board = GameEngine.getEmptyBoard()
+        val player = Player()
+
+        val expected = true
+        val actual = player.canMakeMove(board)
+
+        assertEquals(expected, actual)
+    }
 }
